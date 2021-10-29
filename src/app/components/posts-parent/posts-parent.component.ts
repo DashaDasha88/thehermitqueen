4654a4e3f29b269d4c1366b8ad29bc46503adfa6
Dasha <dasha.kotova.mail@gmail.com>
2021-10-29 13:56:58 -0400

    clean up the styles

--- src/app/components/posts-parent/posts-parent.component.ts
+++ src/app/components/posts-parent/posts-parent.component.ts
@@ -18,7 +18,8 @@ export class PostsParentComponent implements OnInit {
   }
 
   deletePost(post: Post) {
-    this.postService.deletePosts(post).subscribe(() => (this.posts = this.posts.filter((p) => p.id !== post.id)));
+    this.postService.deletePosts(post)
+    .subscribe(() => (this.posts = this.posts.filter((p) => p.id !== post.id)));
   }
 
    addPost(post : Post) {
